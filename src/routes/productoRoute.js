a444933fb68b0b274932435bb01ddef5e0ee1dfc
gcasas@agetic.gob.bo <gcasas@agetic.gob.bo>
2018-05-29 07:36:54 -0400

    first commit

---
+++ src/routes/productoRoute.js
@@ -0,0 +1,12 @@
+'use strict'
+var express = require('express');
+var productoController = require('../controllers/productoController');
+var md_autenticar=require('../middlewares/autenticar');
+ var api = express.Router();
+
+ //md_autenticar.authSeguro
+ api.get('/homeProducto', md_autenticar.authSeguro,productoController.productoHome);
+ api.post('/producto',md_autenticar.authSeguro,productoController.guardarProducto);
+ api.get('/producto/:id',md_autenticar.authSeguro,productoController.getProducto);
+ api.get('/productos/:pagina/:items?',md_autenticar.authSeguro,productoController.listarProductos);
+ module.exports = api;
